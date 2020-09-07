de0bc2b8d24fef6b81d74d5112d009ea2f1ff2d9
Abhinav Anand <abhinav.anand500@gmail.com>
2020-09-07 13:36:10 +0530

    mst correction

--- Algorithm/mst.cpp
+++ Algorithm/mst.cpp
@@ -1,17 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter the size of matrix :: ";
-    cin>>n;
-    int i,j,arr[n][n];
-    cout<<"Enter the matrix :: ";
-    for(i=0;i<n;i++)
+    cout << "Enter the size of matrix :: ";
+    cin >> n;
+    int i, j, arr[n][n];
+    cout << "Enter the matrix :: ";
+    for (i = 0; i < n; i++)
     {
-        for(j=0;j<n;j++)
+        for (j = 0; j < n; j++)
         {
-
+            cin >> arr[i][j];
+            if (arr[i][j] == 0)
+            {
+                arr[i][j] = 999;
+            }
         }
     }
 }
