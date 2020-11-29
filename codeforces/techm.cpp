f0e4a573ac11cadb8074cae22064b6e93237b256
Abhinav Anand <abhinav.anand500@gmail.com>
2020-11-29 19:15:43 +0530

    Done

--- codeforces/techm.cpp
+++ codeforces/techm.cpp
@@ -1,17 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n,i,j;
-    cin>>n;
-    int arr[n];
-    for(i=0;i<n;i++){
-        cin>>arr[i];
+    int i;
+    cin>>input1;
+    for(i=0;i<input1;i++){
+        cin>>input2[i];
     }
-    int ans=arr[0];
+    int ans=input2[0];
     int sum=0;
-    for(i=1;i<n;i++)
+    for(i=1;i<input1;i++)
     {
-        sum=sum+0.1*(arr[i]-ans);
+        sum=sum+0.1*(input2[i]-ans);
     }
     cout<<sum<<endl;
 }
